d5973fd4936cc943677bcb6296c173cd170956d0
mostwise <shxrk@shxrk.dev>
2026-05-30 15:05:21 -0400

    fix(contact): fix organization of RenderCard calls

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,3 @@
-import Link from "next/link";
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import RenderCard from "@/components/RenderCard"
 
 export default function Contact() {
@@ -27,9 +20,24 @@ export default function Contact() {
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-16">
-                {RenderCard("discord", "@imsoftaspowder", "add me on discord! this is my main mode of communication", "https://discord.com/users/1204149467251613756")}
-                {RenderCard("signal", "@wisest.33", "i also use signal if you prefer that over discord, but discord is generally better for keeping in touch", "https://signal.org/")}
-                {RenderCard("email", "dylan@wisest.cc", "email is generally the worst way to get in touch with me, but if you have something important to say then go ahead", "mailto:dylan@wisest.cc")}
+                {RenderCard(
+                    "discord", 
+                    "@imsoftaspowder", 
+                    "add me on discord! this is my main mode of communication", 
+                    "https://discord.com/users/1204149467251613756"
+                )}
+                {RenderCard(
+                    "signal", 
+                    "@wisest.33", 
+                    "i also use signal if you prefer that over discord, but discord is generally better for keeping in touch", 
+                    "https://signal.org/"
+                )}
+                {RenderCard(
+                    "email", 
+                    "dylan@wisest.cc", 
+                    "email is generally the worst way to get in touch with me, but if you have something important to say then go ahead", 
+                    "mailto:dylan@wisest.cc"
+                )}
             </div>
 
             <div>
@@ -42,12 +50,42 @@ export default function Contact() {
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                {RenderCard("github", "@mostwise", "check out some of my cool projects and maybe even contribute if you want", "https://github.com/mostwise")}
-                {RenderCard("spotify", "@dylan", "make fun of my horrible music taste, or admire it, i'm always open to talking about my music", "https://open.spotify.com/user/ip24s6xdgak5k5rjnuqc59qo4")}
-                {RenderCard("steam", "@mostwise", "check out some of the games that i play in my free time", "https://steamcommunity.com/id/mostwise")}
-                {RenderCard("monkeytype", "@dylnn", "check out some of my typing, i haven't been really active recently", "https://monkeytype.com/profile/dylnn")}
-                {RenderCard("mastodon", "@shxrk@defcon.social", "i literally never use this but i might start using it more frequently i kinda forgot about it", "https://defcon.social/@shxrk")}
-                {RenderCard("bluesky", "@0xdyl.bsky.social", "i don't even know if this still exists or works but i don't think i've ever used it either", "https://bsky.app/profile/0xdyl.bsky.social")}
+                {RenderCard(
+                    "github", 
+                    "@mostwise", 
+                    "check out some of my cool projects and maybe even contribute if you want", 
+                    "https://github.com/mostwise"
+                )}
+                {RenderCard(
+                    "spotify", 
+                    "@dylan", 
+                    "make fun of my horrible music taste, or admire it, i'm always open to talking about my music", 
+                    "https://open.spotify.com/user/ip24s6xdgak5k5rjnuqc59qo4"
+                )}
+                {RenderCard(
+                    "steam", 
+                    "@mostwise", 
+                    "check out some of the games that i play in my free time", 
+                    "https://steamcommunity.com/id/mostwise"
+                )}
+                {RenderCard(
+                    "monkeytype", 
+                    "@dylnn", 
+                    "check out some of my typing, i haven't been really active recently", 
+                    "https://monkeytype.com/profile/dylnn"
+                )}
+                {RenderCard(
+                    "mastodon", 
+                    "@shxrk@defcon.social", 
+                    "i literally never use this but i might start using it more frequently i kinda forgot about it", 
+                    "https://defcon.social/@shxrk"
+                )}
+                {RenderCard(
+                    "bluesky", 
+                    "@0xdyl.bsky.social", 
+                    "i don't even know if this still exists or works but i don't think i've ever used it either", 
+                    "https://bsky.app/profile/0xdyl.bsky.social"
+                )}
             </div>
         </div>
     )
